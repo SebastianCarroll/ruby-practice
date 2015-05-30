ea1aafb35e373205fbd190cf8a8bbfabddfc9254
sebastian_carroll <sebastianjamescarroll@gmail.com>
2015-05-30 22:51:17 +1000

    Broken code but don't want to lose the work

--- sudoku_solver/sudoku_swap.rb
+++ sudoku_solver/sudoku_swap.rb
@@ -7,9 +7,30 @@ class Sudoku
 
     def get_duplicate_cells
         dup_cells = []
-        dup_cells << Position.new(1,1)
-        dup_cells << Position.new(1,6)
-        dup_cells << Position.new(4,1)
+        9.times do |i|
+            column = @matrix.column(i)
+            row = @matrix.column(i)
+            dup_cells += duplicates_in_row(column, i)
+            dup_cells += duplicates_in_row(row, i)
+        end
         return dup_cells
+    end
+
+    def duplicates_in_row(row, i)
+        value_hash = get_values_as_hash(row,i)
+        dupes= []
+        value_hash.each do |key,pos|
+            dupes += pos if pos.length > 1
+        end
+        return dupes
+    end
+
+    def get_values_as_hash(row,i)
+        hash = Hash.new []
+        row.each_with_index do |val,col|
+            hash[val] += [Position.new(i, col)]
+        end
+        p hash
+        return hash
     end
 end
--- sudoku_solver/sudoku_swap_spec.rb
+++ sudoku_solver/sudoku_swap_spec.rb
@@ -3,7 +3,7 @@ require './position'
 
 describe Sudoku do
     before :each do
-        @matrix = Matrix[[
+        @matrix = Matrix[
             [4, 6, 2, 5, 7, 1, 9, 8, 3],
             [7, 4, 1, 2, 8, 3, 4, 6, 5],
             [5, 8, 3, 6, 4, 9, 2, 7, 1],
@@ -12,7 +12,7 @@ describe Sudoku do
             [2, 3, 5, 1, 6, 7, 8, 9, 4],
             [1, 7, 6, 9, 2, 4, 3, 5, 8],
             [3, 5, 4, 8, 1, 6, 7, 2, 9],
-            [8, 2, 9, 7, 3, 5, 1, 4, 6]]]
+            [8, 2, 9, 7, 3, 5, 1, 4, 6]]
     end
 
     it "should initialise" do
@@ -26,5 +26,35 @@ describe Sudoku do
         dup_cells << Position.new(1,6)
         dup_cells << Position.new(4,1)
         expect(sudoku.get_duplicate_cells).to match_array(dup_cells)
+    end
+
+    it "should find duplicates cells on rows and columns dynamically" do
+        @matrix = Matrix[
+            [5, 6, 2, 5, 7, 1, 9, 8, 3],
+            [7, 9, 1, 2, 8, 3, 4, 6, 5],
+            [5, 8, 3, 6, 4, 9, 2, 7, 1],
+            [6, 1, 7, 4, 9, 8, 5, 3, 2],
+            [9, 4, 8, 3, 5, 2, 6, 1, 7],
+            [2, 3, 5, 1, 6, 7, 8, 9, 4],
+            [1, 7, 6, 9, 2, 4, 3, 5, 8],
+            [3, 5, 4, 8, 1, 6, 7, 2, 9],
+            [8, 2, 9, 7, 3, 5, 1, 4, 6]]
+        sudoku = Sudoku.new @matrix
+        dup_cells = []
+        dup_cells << Position.new(0,0)
+        dup_cells << Position.new(0,3)
+        dup_cells << Position.new(2,0)
+        expect(sudoku.get_duplicate_cells).to match_array(dup_cells)
+    end
+
+    it "should find duplicates in each row" do
+        sudoku = Sudoku.new @matrix
+        row = [2,3,2]
+        i = 0
+        pos_dup = []
+        pos_dup << Position.new(i, 0)
+        pos_dup << Position.new(i, 2)
+        expect(sudoku.duplicates_in_row(row, i)).to match_array(pos_dup)
+
     end
 end
