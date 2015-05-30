5e3eef5397e2cdb8f3cc2894d7af4ec2814ed350
sebastian_carroll <sebastianjamescarroll@gmail.com>
2015-05-30 21:39:03 +1000

    Added bare bones method to find duplicate entries on rows and columns

--- sudoku_solver/sudoku_swap.rb
+++ sudoku_solver/sudoku_swap.rb
@@ -4,4 +4,12 @@ class Sudoku
     def initialize(matrix)
         @matrix= matrix
     end
+
+    def get_duplicate_cells
+        dup_cells = []
+        dup_cells << Position.new(1,1)
+        dup_cells << Position.new(1,6)
+        dup_cells << Position.new(4,1)
+        return dup_cells
+    end
 end
--- sudoku_solver/sudoku_swap_spec.rb
+++ sudoku_solver/sudoku_swap_spec.rb
@@ -5,7 +5,7 @@ describe Sudoku do
     before :each do
         @matrix = Matrix[[
             [4, 6, 2, 5, 7, 1, 9, 8, 3],
-            [7, 9, 1, 2, 8, 3, 4, 6, 5],
+            [7, 4, 1, 2, 8, 3, 4, 6, 5],
             [5, 8, 3, 6, 4, 9, 2, 7, 1],
             [6, 1, 7, 4, 9, 8, 5, 3, 2],
             [9, 4, 8, 3, 5, 2, 6, 1, 7],
@@ -17,5 +17,14 @@ describe Sudoku do
 
     it "should initialise" do
         sudoku = Sudoku.new @matrix
+    end
+
+    it "should find duplicates cells on rows and columns" do
+        sudoku = Sudoku.new @matrix
+        dup_cells = []
+        dup_cells << Position.new(1,1)
+        dup_cells << Position.new(1,6)
+        dup_cells << Position.new(4,1)
+        expect(sudoku.get_duplicate_cells).to match_array(dup_cells)
     end
 end
