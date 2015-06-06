3256a52884a89c2335124e82dc068d9f07dc2e23
sebastian_carroll <sebastianjamescarroll@gmail.com>
2015-06-06 23:49:28 +1000

    Modified test to have two mistakes that can be swapped

--- sudoku_solver/sudoku_swap_spec.rb
+++ sudoku_solver/sudoku_swap_spec.rb
@@ -25,7 +25,7 @@ describe Sudoku do
             [7, 9, 1, 2, 8, 3, 4, 6, 5],
             [5, 8, 3, 6, 4, 9, 2, 7, 1],
             [6, 1, 7, 4, 9, 8, 5, 3, 2],
-            [9, 4, 8, 3, 5, 2, 6, 1, 7],
+            [9, 4, 8, 3, 4, 2, 6, 1, 7],
             [2, 3, 5, 1, 6, 7, 8, 9, 4],
             [1, 7, 6, 9, 2, 4, 3, 5, 8],
             [3, 5, 4, 8, 1, 6, 7, 2, 9],
@@ -33,6 +33,7 @@ describe Sudoku do
         sudoku = Sudoku.new @matrix
         dup_cells = []
         dup_cells << Position.new(0,0)
+        dup_cells << Position.new(4,4)
         expect(sudoku.get_duplicate_cells).to match_array(dup_cells)
     end
 
