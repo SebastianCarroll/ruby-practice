3d272cbeeef940edf82890aa74b9bbe048716bc4
sebastian_carroll <sebastianjamescarroll@gmail.com>
2015-06-08 01:04:23 +1000

    Moved common parts to setup method and replaced original matrix which was no longer being used

--- sudoku_solver/sudoku_swap_spec.rb
+++ sudoku_solver/sudoku_swap_spec.rb
@@ -4,23 +4,6 @@ require './position'
 describe Sudoku do
     before :each do
         @matrix = Matrix[
-            [4, 6, 2, 5, 7, 1, 9, 8, 3],
-            [7, 4, 1, 2, 8, 3, 4, 6, 5],
-            [5, 8, 3, 6, 4, 9, 2, 7, 1],
-            [6, 1, 7, 4, 9, 8, 5, 3, 2],
-            [9, 4, 8, 3, 5, 2, 6, 1, 7],
-            [2, 3, 5, 1, 6, 7, 8, 9, 4],
-            [1, 7, 6, 9, 2, 4, 3, 5, 8],
-            [3, 5, 4, 8, 1, 6, 7, 2, 9],
-            [8, 2, 9, 7, 3, 5, 1, 4, 6]]
-    end
-
-    it "should initialise" do
-        sudoku = Sudoku.new @matrix
-    end
-
-    it "should find overlapping duplicate values" do
-        @matrix = Matrix[
             [5, 6, 2, 5, 7, 1, 9, 8, 3],
             [7, 9, 1, 2, 8, 3, 4, 6, 5],
             [5, 8, 3, 6, 4, 9, 2, 7, 1],
@@ -30,33 +13,34 @@ describe Sudoku do
             [1, 7, 6, 9, 2, 4, 3, 5, 8],
             [3, 5, 4, 8, 1, 6, 7, 2, 9],
             [8, 2, 9, 7, 3, 5, 1, 4, 6]]
-        sudoku = Sudoku.new @matrix
+        @sudoku = Sudoku.new @matrix
+    end
+
+    it "should find overlapping duplicate values" do
         dup_cells = []
         dup_cells << Position.new(0,0,5)
         dup_cells << Position.new(4,4,4)
-        expect(sudoku.get_duplicate_cells).to match_array(dup_cells)
+        expect(@sudoku.get_duplicate_cells).to match_array(dup_cells)
     end
 
     it "should find duplicates in a column" do
-        sudoku = Sudoku.new @matrix
         i = 0
         col = [Position.new(0, i, 2),
                Position.new(1, i, 3),
                Position.new(2, i, 2)]
         expected_dups = col.select{|pos| pos.val == 2}
-        actual_dups = sudoku.duplicates(col)
+        actual_dups = @sudoku.duplicates(col)
         expect(actual_dups).to match_array(expected_dups)
     end
 
     it "should find duplicates in a row" do
-        sudoku = Sudoku.new @matrix
         i = 0
         row = [Position.new(i, 0, 5),
                Position.new(i, 1, 3),
                Position.new(i, 2, 3),
                Position.new(i, 3, 2)]
         expected_dups = row.select{|pos| pos.val == 3 }
-        actual_dups = sudoku.duplicates(row)
+        actual_dups = @sudoku.duplicates(row)
         expect(actual_dups).to match_array(expected_dups)
     end
 end
