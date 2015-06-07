946363be1356b0e45a73c552097af3d9c745cca9
sebastian_carroll <sebastianjamescarroll@gmail.com>
2015-06-08 09:33:41 +1000

    Added test to correctly identify a solved (correct) sudoku

--- sudoku_solver/sudoku_swap_spec.rb
+++ sudoku_solver/sudoku_swap_spec.rb
@@ -43,4 +43,19 @@ describe Sudoku do
         actual_dups = @sudoku.duplicates(row)
         expect(actual_dups).to match_array(expected_dups)
     end
+
+    it "should recognize a correctly solved sudoku" do
+        expect(@sudoku.correct?).to be false
+        sudoku2 = Sudoku.new(Matrix[
+            [4, 6, 2, 5, 7, 1, 9, 8, 3],
+            [7, 9, 1, 2, 8, 3, 4, 6, 5],
+            [5, 8, 3, 6, 4, 9, 2, 7, 1],
+            [6, 1, 7, 4, 9, 8, 5, 3, 2],
+            [9, 4, 8, 3, 5, 2, 6, 1, 7],
+            [2, 3, 5, 1, 6, 7, 8, 9, 4],
+            [1, 7, 6, 9, 2, 4, 3, 5, 8],
+            [3, 5, 4, 8, 1, 6, 7, 2, 9],
+            [8, 2, 9, 7, 3, 5, 1, 4, 6]])
+        expect(sudoku2.correct?).to be true
+    end
 end
