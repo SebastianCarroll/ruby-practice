b887fa614d1128ef41f5f3c11ad55fbb37cb6f1f
sebastian_carroll <sebastianjamescarroll@gmail.com>
2015-10-15 19:54:57 +1100

    Added x minutes too hour

--- the_time_in_words/read_time.rb
+++ the_time_in_words/read_time.rb
@@ -12,6 +12,24 @@ class TimeReader
         10 => "ten",
         11 => "eleven",
         12 => "twelve",
+        13 => "thirteen",
+        14 => "fourteen",
+        15 => "fifteen",
+        16 => "sixteen",
+        17 => "seventeen",
+        18 => "eighteen",
+        19 => "ninteen",
+        20 => "twenty",
+        21 => "twenty one",
+        22 => "twenty two",
+        23 => "twenty three",
+        24 => "twenty four",
+        25 => "twenty five",
+        26 => "twenty six",
+        27 => "twenty seven",
+        28 => "twenty eight",
+        29 => "twenty nine",
+        30 => "thirty",
     }
 
     def initialize(hour, minutes)
@@ -28,6 +46,9 @@ class TimeReader
             "#{D_TO_S[@hour]} o' clock"
         elsif @minutes == 30
             "half past #{D_TO_S[@hour]}"
+        elsif @minutes > 30
+            diff = 60-@minutes
+            "#{D_TO_S[diff]} minutes to #{D_TO_S[@hour+1]}"
         else
             "#{D_TO_S[@minutes]} minute#{plural_min} past #{D_TO_S[@hour]}"
         end
--- the_time_in_words/spec/read_time_spec.rb
+++ the_time_in_words/spec/read_time_spec.rb
@@ -8,6 +8,7 @@ describe TimeReader do
         [5,01,"one minute past five"],
         [5,10,"ten minutes past five"],
         [5,30,"half past five"],
+        [5,40,"twenty minutes to six"],
     ].each do |input|
         hour= input[0]
         min= input[1]
