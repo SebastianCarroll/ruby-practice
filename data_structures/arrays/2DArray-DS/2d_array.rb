631314fbc7c94768007b7df707d98fc2a299c6ef
Sebastian Carroll <seabass25@hotmail.com>
2016-01-19 21:23:56 +1100

    Added more general solution to larger array

--- data_structures/arrays/2DArray-DS/2d_array.rb
+++ data_structures/arrays/2DArray-DS/2d_array.rb
@@ -1,6 +1,25 @@
 Array2D = Struct.new :input do
   def find_highest_hourglass
-    return 7
+    t = input.length
+    max = 0
+    1.upto(t-2) do |x|
+      1.upto(t-2) do |y|
+        max = [max, get_hourglass(x,y).inject(:+)].max
+      end
+    end
+    max
+  end
+
+  def get_hourglass(x,y)
+    [
+      input[x-1][y-1],
+      input[x-1][y],
+      input[x-1][y+1],
+      input[x][y],
+      input[x+1][y-1],
+      input[x+1][y],
+      input[x+1][y+1],
+    ]
   end
 end
 
--- data_structures/arrays/2DArray-DS/2d_array_test.rb
+++ data_structures/arrays/2DArray-DS/2d_array_test.rb
@@ -12,4 +12,15 @@ describe Array2D do
     highest = Array2D.new(input).find_highest_hourglass
     highest.must_equal 7
   end
+
+  it "should find answer for 4x4 square" do
+    input = [
+      [1,1,1,2],
+      [0,1,1,0],
+      [1,1,1,2],
+      [1,1,1,2],
+    ]
+    highest = Array2D.new(input).find_highest_hourglass
+    highest.must_equal 9
+  end
 end
