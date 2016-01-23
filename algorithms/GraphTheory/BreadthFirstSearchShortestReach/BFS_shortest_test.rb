f50aa19f0adc86b787b829cc75460ef6d043d144
Sebastian Carroll <seabass25@hotmail.com>
2016-01-23 11:10:27 +1100

    Added bare files for BFS shortest

---
+++ algorithms/GraphTheory/BreadthFirstSearchShortestReach/BFS_shortest_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require 'pry'
+require 'pry-nav'
+require_relative "BFS_shortest"
+
+describe BFS_Shortest do
+  before do
+  end
+
+  it "should something useful" do
+    true.must_equal false
+  end
+end
